a24228d470a02737796d3490cb6eeef9b774f24e
adelbouras <eyyad.kareem1212@gmail.com>
2025-09-10 17:25:05 +0100

    init

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"JetBrainsMono Nerd Font:size=15 , Noto Sans Arabic:size=16"
@@ -9,12 +9,12 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#bbbbbb", "#e53212" },
+	[SchemeSel] = { "#ffffff", "#f67434" },
+	[SchemeOut] = { "#000000", "#2d3d5d" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 7;
 
 /*
  * Characters not considered part of a word while deleting words
