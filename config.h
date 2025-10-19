b5305c13a578d4b9383ee4c01d0f0e066a4fb2f4
adelbouras <eyyad.kareem1212@gmail.com>
2025-10-19 14:41:06 +0100

    add: patch some

--- config.h
+++ config.h
@@ -1,20 +1,20 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0; /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrainsMono Nerd Font:size=15 , Noto Sans Arabic:size=16"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+    "JetBrainsMono Nerd Font:size=12 , Noto Sans Arabic:size=16"};
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#e53212" },
-	[SchemeSel] = { "#ffffff", "#f67434" },
-	[SchemeOut] = { "#000000", "#2d3d5d" },
+    /*                   fg         bg       */
+    [SchemeNorm] = {"#bbbbbb", "#e53212"},
+    [SchemeSel] = {"#ffffff", "#f67434"},
+    [SchemeOut] = {"#000000", "#2d3d5d"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 7;
+static unsigned int lines = 7;
 
 /*
  * Characters not considered part of a word while deleting words
